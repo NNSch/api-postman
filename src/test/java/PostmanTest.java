136788d1b1841df175e831c8f664aad9beb2f6bc
Natalia Shevchenko <ledidile@rambler.ru>
2023-08-29 22:22:17 +0300

    commit phone

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    public void test(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("phone") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("phone"))
+        ;
+
+    }
+}
